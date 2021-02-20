2db948572adf56073ac3594764d1d23fe10b32f3
AlfonsoOchoa <79351964+AlfonsoOchoa@users.noreply.github.com>
2021-02-19 22:53:01 -0800

    Added home page and header component

---
+++ pages/home.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header.jsx";
+import Description from "../components/Description.jsx";
+
+const Home = function () {
+  return (
+    <>
+      <Header />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Description />
+      </div>
+    </>
+  );
+};
+export default Home;
