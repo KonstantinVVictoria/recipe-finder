54ffe539b6fa3af676d6da8c47934a6c06b15445
saad <saadrehman2000@gmail.com>
2021-02-20 13:03:30 +0400

    ML model

---
+++ components/SubmitImage.jsx
@@ -0,0 +1,11 @@
+const SubmitImage = () => {
+    return (
+        <div>
+            <input type="button" value="x" style={{marginLeft: "500px", marginTop: "50px", marginBottom: "0px"}} ></input>
+            <h1 style={{ marginTop: "0px", marginRight: "500px", fontWeight: "normal"}}>Upload an image</h1>
+            <h3 style={{marginTop: "40px"}}>jkbdwkbkwebdbwed</h3>
+        </div>
+    );
+}
+
+export default SubmitImage;
