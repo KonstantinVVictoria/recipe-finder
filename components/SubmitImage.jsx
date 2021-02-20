72dbe3c0938054ff758f06d21d0dc0437a1eb71e
saad <saadrehman2000@gmail.com>
2021-02-20 14:46:28 +0400

    SubmitImage component work

--- components/SubmitImage.jsx
+++ components/SubmitImage.jsx
@@ -1,9 +1,21 @@
+import Theme from "../styles/Theme.js";
+
 const SubmitImage = () => {
     return (
-        <div>
-            <input type="button" value="x" style={{marginLeft: "500px", marginTop: "50px", marginBottom: "0px"}} ></input>
-            <h1 style={{ marginTop: "0px", marginRight: "500px", fontWeight: "normal"}}>Upload an image</h1>
-            <h3 style={{marginTop: "40px"}}>jkbdwkbkwebdbwed</h3>
+        <div style={{ backgroundColor: "beige" }}>
+            <input type="button" value="X" class="closeButton" style={{ marginLeft: "680px", marginTop: "10px", marginBottom: "0px", backgroundColor: "transparent", borderRadius: "7px", borderColor: "black"}} ></input>
+            <h1 style={{ marginTop: "0px", marginRight: "500px", fontWeight: "normal" }}>Upload an image</h1>
+            <h3 style={{ marginTop: "40px", fontWeight: "lighter" }}>Pick a food of your choosing, upload and submit for a list of similar dishes.</h3>
+            <div style={{ borderRadius: "15px", border: "7px solid gray", marginLeft: "60px" , marginRight: "340px"}}>
+                <p>.</p>
+                <p>.</p>
+                <p>.</p>
+                <p>.</p>
+                <p style={{marginLeft: "45px"}}><b>Drop & Drag</b> a file here or click...</p>
+                <input type="button" value="Upload" style={{marginBottom: "20px", border: "none", marginLeft: "110px", fontSize: 17, borderRadius: "15px", backgroundColor: "salmon", padding: "5px 20px"}}></input>
+            </div>
+            <input type="button" value="Submit" style={{marginBottom: "20px", border: "none", marginLeft: "605px", fontSize: 17, borderRadius: "15px", backgroundColor: "salmon", padding: "5px 20px"}}></input>
+
         </div>
     );
 }
--- styles/Theme.js
+++ styles/Theme.js
@@ -1,7 +1,7 @@
-export default Theme = {
+export default  {
   beige: "FFF4DF",
   salmon: "FD8664",
   yellow: "FABE0E",
   green: "8AB661",
-  gray: "4B5454",
+  gray: "4B5454"
 };
