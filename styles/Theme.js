2465a6bee05d657b4fd0dc0fdae4d34135f0b822
Konstantin Victoria <61480260+KonstantinVVictoria@users.noreply.github.com>
2021-02-20 16:08:16 -0800

    commit fix --package.json

--- styles/Theme.js
+++ styles/Theme.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 export default {
-=======
-export default  {
->>>>>>> 543c7796d8d9ed556050c2f138c6961dfc4bd35d
   beige: "FFF4DF",
   salmon: "FD8664",
   yellow: "FABE0E",
   green: "8AB661",
-  gray: "4B5454"
+  gray: "4B5454",
 };
